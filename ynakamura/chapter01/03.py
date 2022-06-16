9a5984098fe81e18abf77f695e24de69bd8f25b1
中村優希 <nakamurayuki@nakamurayuukinoMacBook-Pro.local>
2022-06-16 16:55:47 +0900

    03.pyと05.pyを修正

--- ynakamura/chapter01/03.py
+++ ynakamura/chapter01/03.py
@@ -3,4 +3,4 @@ import wavio
 
 
 wav = wavio.read("sin_fin440_fs16k.wav")
-wavio.write("sin_fin440_fs8k.wav", wav.data[0::2], int(wav.rate / 2), sampwidth=2)
+wavio.write("sin_fin440_fs8k.wav", wav.data[0::2], wav.rate // 2, sampwidth=2)
--- ynakamura/chapter01/05.py
+++ ynakamura/chapter01/05.py
@@ -6,7 +6,7 @@ fs = 16000
 sec = 3
 t = np.linspace(0., sec, int(fs * sec))
 
-whitenoise = np.random.rand(int(fs * sec)) * 2 - 1
+whitenoise = np.random.rand(fs * sec) * 2 - 1
 
 plt.plot(t, whitenoise)
 plt.xlim(0, 0.03)
