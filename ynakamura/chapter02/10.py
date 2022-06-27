23b398ba39212f23b023d8f78658d77455e01f3a
中村優希 <nakamurayuki@nakamurayuukinoMacBook-Pro.local>
2022-06-27 10:15:09 +0900

    不要なコメントアウトの削除

--- ynakamura/chapter02/10.py
+++ ynakamura/chapter02/10.py
@@ -9,13 +9,9 @@ def hamming(N):
 
 
 def convolve(a, v):
-    # (a * v)[n] = \sum_{m=-\infty}^{\infty} a[m] v[n - m] (n = 1, ..., N)
     a_len = a.shape[0]
     v_len = v.shape[0]
     sum = np.array([])
-    # for n in range(a_len):
-    #     for m in range(v_len):
-    #         sum += a[m] * v[n - m]
     for i in range(a_len - v_len + 1):
         sum = np.append(sum, np.sum(a[i : v_len + i] * np.flip(v)))
     return sum
@@ -34,7 +30,6 @@ W = np.fft.fft(w)
 
 X = np.pad(X, (X.shape[0] // 2, X.shape[0] // 2 - 1))
 
-# x_ham = np.fft.ifft(np.convolve(X, W, mode="valid"))  # 動作確認用
 x_ham = np.fft.ifft(convolve(X, W))
 
 plt.plot(t, x_ham)
