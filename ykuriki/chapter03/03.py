f4dcdccf9f1e7a618e67bb5ec46b9f7640141b0d
y.kuriki <kuriki.yui@gmail.com>
2022-07-04 02:32:42 +0900

    chapter03追加しました

---
+++ ykuriki/chapter03/03.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+# 関数
+def circul_conv_pad(x, h):
+    """x[n]とh[n]の巡回たたみこみを計算する（非周期的）
+
+    Args:
+        x (ndarray): 信号（1次元配列）
+        h (ndarray): インパルス応答（1次元配列）
+
+    Returns:
+        y (ndarray): 畳み込み結果（1次元配列）
+
+    """
+    x_size = len(x)
+    h_size = len(h)
+    y_size = x_size + h_size - 1
+
+    X = np.fft.fft(np.block([x, np.zeros(h_size)]))
+    H = np.fft.fft(np.block([h, np.zeros(x_size)]))
+    y = np.fft.ifft(X * H)
+    y = y[:y_size]
+
+    return y
