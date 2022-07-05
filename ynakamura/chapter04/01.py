b65b11d20c3c3f6bcb0de47d9e080a27b716609b
中村優希 <nakamurayuki@nakamurayuukinoMacBook-Pro.local>
2022-07-05 16:58:17 +0900

    add: chapter04

---
+++ ynakamura/chapter04/01.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+
+def zero_pad(x, L, S):
+    x_pad = np.pad(x, L - S)
+    if x_pad.shape[0] % S != 0:
+        x_pad = np.pad(x_pad, ((0, S - (x_pad.shape[0] % S))))
+    return x_pad
