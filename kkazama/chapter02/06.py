a86eb4a6a89f328a02c99083d97e0ad8f9c7681c
todayka <todayka.sd.0921@gmail.com>
2022-07-07 23:50:15 +0900

    revise chapter02

--- kkazama/chapter02/06.py
+++ kkazama/chapter02/06.py
@@ -7,20 +7,21 @@ f = 440  # 周波数
 sec = 3  # 長さ
 fs = 16000  # サンプリング周波数
 
-t = np.arange(0, sec, 1 / fs)  # 時間インデックス
+t = np.arange(0, sec * fs) / fs  # 時間インデックス
 
 xt = A * np.sin(2 * np.pi * f * t)  # 正弦波
 
-xt_db = 20 * np.log10(np.abs(xt))
-
 Xk = np.fft.fft(xt)
+
+amp = 20 * np.log10(np.abs(Xk))
+angle = 20 * np.log(np.angle(Xk))
 
 # 振幅スペクトル
 plt.subplot(2, 1, 1)
-plt.stem(np.abs(Xk))
+plt.stem(amp)
 
 # 位相スペクトル
 plt.subplot(2, 1, 2)
-plt.stem(np.angle(Xk))
+plt.stem(angle)
 
 plt.show()
