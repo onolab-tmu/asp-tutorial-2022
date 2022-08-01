0efcf09cba25ab1adda1f60c4664baf83bc4c8b4
daiki sugawara <dksuga3652@gmail.com>
2022-08-01 11:59:06 +0900

    Revert "Merge branch 'main' of https://github.com/dksuga11/asp-tutorial-2022"
    This reverts commit eaa80c1f999e37aaf93d5e0e046277d645927aba, reversing
    changes made to cc9b33c53ba73656aee9a40cb80d786e7349aacb.

--- ynakamura/chapter04/01.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-def zero_pad(x, L, S):
-    x_pad = np.pad(x, L - S)
-    if x_pad.shape[0] % S != 0:
-        x_pad = np.pad(x_pad, ((0, S - (x_pad.shape[0] % S))))
-    return x_pad
